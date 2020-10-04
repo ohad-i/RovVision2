74185969349cc784bfc84cebc978cd812fe0258e
OI <ohadon@gmail.com>
2020-10-04 07:36:40 +0300

    update

--- scripts/sync_onboard.sh
+++ scripts/sync_onboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #rsync -avzu -e "ssh -p 2222" --exclude="*.AppImage*" --exclude="*.mp4" --exclude="*.pyc" --exclude=".git/" . stereo@localhost:/home/stereo/bluerov/
-rsync -avzu -e "ssh -p 2222"  --exclude=".git" --include="*/" --include="*.c" --include="*.sh" --include="*.py" --include="*.ino" --exclude="*" $HOME/RovVision2/ stereo@localhost:/home/stereo/RovVision2/
+rsync -avzu --exclude=".git" --include="*/" --include="*.c" --include="*.sh" --include="*.py" --include="*.ino" --exclude="*" $HOME/proj/RovVision2/ nanosub@192.168.2.10:/home/nanosub/proj/RovVision2/
 sleep 2
