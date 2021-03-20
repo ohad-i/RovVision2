143ccd13c1b8ed89ecdb295030dd1ec37f9c6b14
ohadi <ohadon@gmail.com>
2021-03-20 12:51:21 +0200

    update script to fetch records from ROV

--- scripts/recSync.sh
+++ scripts/recSync.sh
@@ -3,6 +3,7 @@
 tmux kill-session -t syncRec
 tmux new-session -d -s syncRec
 
-tmux send-keys "rsync -avzu --progress --remove-source-files --exclude=".git" --include="*/"  --include="*.pkl" --include="*.bin" --exclude="*" $REMOTE_SUB:/home/nanosub/proj/RovVision2/records $HOME/proj/RovVision2/" ENTER
-tmux send-keys "sleep 300" ENTER
-tmux send-keys "tmux kill-session -t syncRec" ENTER
+tmux send-keys -t syncRec "rsync -avzu --progress --remove-source-files --exclude=".git" --include="*/"  --include="*.pkl" --include="*.bin" $REMOTE_SUB:/home/nanosub/proj/RovVision2/records $HOME/proj/RovVision2/" ENTER
+tmux send-keys -t syncRec "sleep 30" ENTER
+tmux send-keys -t syncRec "tmux kill-session -t syncRec" ENTER
+tmux attach -d
