30b4002ba4328e8ea7d88fbf343dd7248d1b95ac
ohadi <ohadon@gmail.com>
2021-03-20 14:04:22 +0200

    player integrated with rovViewer

--- utils/player.py
+++ utils/player.py
@@ -24,6 +24,7 @@ parser.add_argument('-r', '--recPath', default=None, help=' path to record ')
 parser.add_argument('-s', '--skipFrame', type=int, default=-1, help='start of parsed frame, by frame counter not file index')
 parser.add_argument('-t', '--saveTiff', action='store_true', help='save tiffs files to record folder')
 parser.add_argument('-q', '--showVideo', action='store_false', help='quite run, if -q - parse only, no show')
+parser.add_argument('-V', '--saveAvi', action='store_true', help='quite run, if -V - create avi files')
 
 args = parser.parse_args()
 
@@ -32,6 +33,7 @@ recPath = args.recPath
 skipFrame = args.skipFrame 
 saveTiff = args.saveTiff
 showVideo = args.showVideo
+saveAvi = args.saveAvi
 
 
 print(usageDescription)
@@ -53,7 +55,9 @@ def CallBackFunc(event, x, y, flags, params):
 
 if showVideo:
     winName = 'player'
+    winNameLowRes = 'player - low Res'
     cv2.namedWindow(winName, 0)
+    cv2.namedWindow(winNameLowRes, 0)
     cv2.setMouseCallback(winName, CallBackFunc)
 
 
@@ -86,7 +90,7 @@ def vidProc(im, imLowRes, imPub = None):
                    fontScale, color, thickness, cv2.LINE_AA)
 
 
-        if writer is None and im is not None:
+        if saveAvi and writer is None and im is not None:
             (h, w) = showIm.shape[:2]
             ### option for uncompressed raw
             #fourcc = cv2.VideoWriter_fourcc(*'DIB ')
@@ -98,7 +102,7 @@ def vidProc(im, imLowRes, imPub = None):
             writer = cv2.VideoWriter(vidFileName, fourcc, 2,
                                              (w, h), True)
             writer.write(showIm)
-        elif im is not None:
+        elif saveAvi and im is not None:
             writer.write(showIm)
 
     
@@ -114,20 +118,7 @@ def vidProc(im, imLowRes, imPub = None):
     
         if showVideo:
             cv2.imshow(winName, showIm) #im[:200,:])
-
-            key = cv2.waitKey(curDelay)&0xff
-            if key == ord('q'):
-                return False
-            elif key == ord(' '):
-                curDelay = 0
-            elif key == ord('+'):
-                curDelay = max(1, curDelay-5 )
-            elif key == ord('-'):
-                curDelay = min(1000, curDelay+5 )
-            elif key == ord('r'):
-                curDelay = 100
-        else:
-            print('current frame process %d'%frameId)
+            
 
     if imLowRes is not None:
         imLowRes = cv2.cvtColor(imLowRes, cv2.COLOR_BGR2RGB)
@@ -135,7 +126,7 @@ def vidProc(im, imLowRes, imPub = None):
         showImLow = cv2.putText(showImLow, '%04d '%(frameId), org, font,
                    fontScale/2, color, thickness, cv2.LINE_AA)
 
-        if writerLowRes is None:
+        if saveAvi and writerLowRes is None:
             (h, w) = showImLow.shape[:2]
             ### option for uncompressed raw
             #fourcc = cv2.VideoWriter_fourcc(*'DIB ')
@@ -147,9 +138,29 @@ def vidProc(im, imLowRes, imPub = None):
             writerLowRes = cv2.VideoWriter(vidFileName, fourcc, 10,
                                                 (w, h), True)
             writerLowRes.write(showImLow)
-        else:
+        elif saveAvi:
             writerLowRes.write(showImLow)
+            
+        if showVideo:
+            cv2.imshow(winNameLowRes, showImLow) #im[:200,:])
 
+            
+
+    if showVideo:
+        key = cv2.waitKey(curDelay)&0xff
+        if key == ord('q'):
+            return False
+        elif key == ord(' '):
+            curDelay = 0
+        elif key == ord('+'):
+            curDelay = max(1, curDelay-5 )
+        elif key == ord('-'):
+            curDelay = min(1000, curDelay+5 )
+        elif key == ord('r'):
+            curDelay = 100
+    else:
+        pass 
+        #print('current frame process %d'%frameId)
 
     return True
 
@@ -166,13 +177,16 @@ if __name__=='__main__':
         revTopicPubDict = {}
         for topic in topicsList:
             if topic[1] in pubList: # port already exist
-                print('reuse publisher port: %d'%topic[1])
+                print('reuse publisher port: %d %s'%(topic[1], topic[0]) )  
                 topicPubDict[topic[0]] = topicPubDict[revTopicPubDict[topic[1]]]
             else:
-                print('creats publisher on port: %d'%topic[1])
+                print('creats publisher on port: %d %s'%(topic[1], topic[0]) )
                 topicPubDict[topic[0]] = utils.publisher(topic[1])
                 revTopicPubDict[topic[1]] = topic[0]
                 pubList.append(topic[1])
+        
+        fpsTic = time.time()
+        fpsCnt = 0.0
         
         if recPath is not None:
             vidPath = os.path.join(recPath, 'video.bin')
@@ -183,69 +197,84 @@ if __name__=='__main__':
             highResEndFlag = False
             lowResEndFlag = False
             telemRecEndFlag = False
-            with open(vidPath, 'rb') as vidFid:
-                with open(vidQPath, 'rb') as vidQFid:
-                    with open(telemPath, 'rb') as telFid:
-         
-                        while True:
-                            try:
-                                data = pickle.load(telFid)
-                            except:
-                                if not telemRecEndFlag:
-                                    print('record Ended...')
-                                    telemRecEndFlag = True
-                                    break
-                                
-        
-                            curTopic = data[1][0]
-                            #print('curTopic',curTopic)
-        
-                            if curTopic == zmq_topics.topic_stereo_camera:
-                                frameId += 1
-                                ## handle image
-                                metaData = pickle.loads(data[1][1])
-                                imShape  = metaData[1]
-                                imgCnt  += 1
-                                # load image
-                                try:
-                                    '''
-                                    imLowRes = np.fromfile(vidQFid, count=imShape[1]//2*imShape[0]//2*imShape[2], 
-                                                           dtype = 'uint8').reshape((imShape[1]//2, imShape[0]//2, imShape[2] ))
-                                    '''
-                                    imLowRes = None
-                                    
-                                    
-                                except:
-                                    imLowRes = None
-                                    if not lowResEndFlag:
-                                        print('low res video ended')
-                                        lowResEndFlag = True
-                                if imgCnt%4 == 0:
-                                    try:
-                                        imRaw = np.fromfile(vidFid, count=imShape[1]*imShape[0]*imShape[2], dtype = 'uint8').reshape(imShape)
-                                    except:
-                                        imRaw = None
-                                        if not highResEndFlag:
-                                            print('high res video ended')
-                                            highResEndFlag = True
-                                else:
-                                    imRaw = None
-                                ret = vidProc(imRaw, imLowRes)
-                               
-                                if not ret:
-                                    break
+            telFid = open(telemPath, 'rb')
+            vidFid = open(vidPath, 'rb')
+            vidQFid = open(vidQPath, 'rb')
+            
+            
+            curData = pickle.load(telFid)
+            nextData = pickle.load(telFid)
+            nextTicToc = 0.9*(nextData[0] - curData[0]) 
+            while True:
+                try:
+                    data = curData #pickle.load(telFid)
+                    
+                except:
+                    if not telemRecEndFlag:
+                        print('record Ended...')
+                        telemRecEndFlag = True
+                        break
+                    
 
-                                #import ipdb; ipdb.set_trace()
-                                #recTs, telData = pickle.loads(data[1][1])
-                                
-                                topicPubDict[curTopic].send_multipart(data[1])
-                            else:
-                                #print(topic, data[1])
-                                recTs, telData = pickle.loads(data[1][1])
-                                topicPubDict[curTopic].send_multipart([curTopic, pickle.dumps(telData)] )
-                                
-                                #pass
-                                #topicPubDict[curTopic].send_multipart(data[1])
+                curTopic = data[1][0]
+                if curTopic == zmq_topics.topic_stereo_camera:
+                    fpsCnt+=1
+                    if time.time() - fpsTic >= 5:
+                        fps = fpsCnt/(time.time() - fpsTic)
+                        print('player video fps %0.2f'%fps)
+                        fpsCnt = 0.0
+                        fpsTic = time.time()
+                        
+                    frameId += 1
+                    ## handle image
+                    metaData = pickle.loads(data[1][1])
+                    hasHighRes = data[1][-1]
+                    
+                    imShape  = metaData[1]
+                    imgCnt  += 1
+                    # load image
+                    try:
+                        
+                        imLowRes = np.fromfile(vidQFid, count=imShape[1]//2*imShape[0]//2*imShape[2], 
+                                               dtype = 'uint8').reshape((imShape[0]//2, imShape[1]//2, imShape[2] ))
+                    except:
+                        imLowRes = None
+                        if not lowResEndFlag:
+                            print('low res video ended')
+                            lowResEndFlag = True
+                    if hasHighRes:
+                        try:
+                            imRaw = np.fromfile(vidFid, count=imShape[1]*imShape[0]*imShape[2], dtype = 'uint8').reshape(imShape)
+                        except:
+                            imRaw = None
+                            if not highResEndFlag:
+                                print('high res video ended')
+                                highResEndFlag = True
+                    else:
+                        imRaw = None
+                    ret = vidProc(imRaw, imLowRes)
+                   
+                    if not ret:
+                        break
+
+                    #import ipdb; ipdb.set_trace()
+                    videoMsg = [zmq_topics.topic_stereo_camera,
+                                        pickle.dumps((metaData[0], imLowRes.shape, metaData[2])),
+                                            imLowRes.tobytes()] # [topic, (frameId, frameShape, ts) rawFrame]
+                    #print('-->', curTopic)
+                    topicPubDict[curTopic].send_multipart(videoMsg)
+                else:
+                    recTs = data[0]
+                    telData = pickle.loads(data[1][1])
+                    topicPubDict[curTopic].send_multipart([curTopic, pickle.dumps(telData)] )
+                    
+                    #pass
+                    #topicPubDict[curTopic].send_multipart(data[1])
+            
+                curData = nextData
+                time.sleep(nextTicToc)
+                nextData = pickle.load(telFid)
+                nextTicToc = 0.9*(nextData[0] - curData[0])
                     
                       
     except:
