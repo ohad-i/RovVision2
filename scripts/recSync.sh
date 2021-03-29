0b3252f8266368ab3e7e1dd26ec05afb3b93798f
ohadi <ohadon@gmail.com>
2021-03-29 14:31:24 +0300

    record sync restart from the last point of failure

--- scripts/recSync.sh
+++ scripts/recSync.sh
@@ -3,7 +3,7 @@
 tmux kill-session -t syncRec
 tmux new-session -d -s syncRec
 
-tmux send-keys -t syncRec "rsync -avzu --progress --remove-source-files --exclude=".git" --include="*/"  --include="*.pkl" --include="*.bin" $REMOTE_SUB:/home/nanosub/proj/RovVision2/records $HOME/proj/RovVision2/" ENTER
+tmux send-keys -t syncRec "rsync -avzu --progress --partial-dir=.rsync-partial --remove-source-files --exclude=".git" --include="*/"  --include="*.pkl" --include="*.bin" $REMOTE_SUB:/home/nanosub/proj/RovVision2/records $HOME/proj/RovVision2/" ENTER
 tmux send-keys -t syncRec "sleep 30" ENTER
 tmux send-keys -t syncRec "tmux kill-session -t syncRec" ENTER
 tmux attach -d
