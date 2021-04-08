f33b6fb39b881ac899600ef625a7c7fcb32d02d1
ohadi <ohadon@gmail.com>
2021-04-08 13:19:34 +0300

    update script for update pids

--- scripts/updateRemotePIDs.sh
+++ scripts/updateRemotePIDs.sh
@@ -4,11 +4,11 @@ tmux kill-session -t updatePIDS
 tmux new-session -d -s updatePIDS
 
 CMD="pkill -f plugin.py"
-./sync_onboard.sh && sleep 2 && ssh -t $REMOTE_SUB $CMD
+#./sync_onboard.sh && sleep 2 && ssh -t $REMOTE_SUB $CMD
 
-tmux send-keys -t updatePIDS "./sync_onboard.sh && ssh -t $REMOTE_SUB $CMD" ENTER
-tmux attach -d
+tmux send-keys -t updatePIDS "./sync_onboard.sh && ssh -t $REMOTE_SUB $CMD && tmux kill-session -t updatePIDS" ENTER
+#tmux attach -d
 
-tmux kill-session -t updatePIDS
+
 
 
