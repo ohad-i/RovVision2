74258d3d75cb36ecf37bfb44ae14c9298db2bb08
ohadi <ohadon@gmail.com>
2021-04-12 19:45:19 +0300

    update

--- scripts/runAutofocus.sh
+++ scripts/runAutofocus.sh
@@ -8,5 +8,7 @@ PROJECT_PATH=/home/nanosub/proj/RovVision2/
 
 tmux send-keys -t $SESSION "cd $PROJECT_PATH/utils" ENTER
 tmux send-keys -t $SESSION "python3 autoFocus.py" ENTER
+
+#tmux send-keys -t $SESSION "tmux kill-session -t $SESSION" ENTER
 #tmux attach -d
 
