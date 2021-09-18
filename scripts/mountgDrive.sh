0bce951ed54fc4f4a555556cd4ceda04a8805331
ohadi <ohadon@gmail.com>
2021-09-18 14:22:14 +0300

    update scripts

---
+++ scripts/mountgDrive.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+MOUNT_PNT=~/gDrive/
+
+if [ $# -eq 0 ]
+  then
+    echo "usage: "
+    echo "To mount known google drive: " 
+    echo "                         ./mountgDrive.sh label"
+    echo "mount point is: $MOUNT_PNT"
+    echo ""
+    echo ""
+    echo ""
+    echo "need to set a label, currnt available lables are:"
+    ls ~/.gdfuse | sort -n
+    exit 1
+fi
+
+if [ "$1" = "Umnt" ]; then
+    echo ""
+    echo "unmount google drive..."
+    echo ""
+    fusermount -u $MOUNT_PNT 
+    exit 1
+fi
+ 
+if [ ! -d "$MOUNT_PNT" ]; then
+    echo "mount point doesnt exist, creats it..."
+    mkdir -p $MOUNT_PNT
+fi 
+echo ""
+echo "mounting drive to $MOUNT_PNT... To unount any drive: "
+echo "                                   ./mountgDrive.sh Umnt"
+ 
+
+google-drive-ocamlfuse -label $1 $MOUNT_PNT
